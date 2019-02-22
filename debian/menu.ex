f3181312ad0a4fd90328ae4618d19e99fd5e5265
~0mega <crt0mega@c-r-t.tk>
2019-02-22 17:27:52 +0000

    Removed old deb packaging info

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(protontricks):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="protontricks" command="/usr/bin/protontricks"
--- debian/protontricks.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the protontricks package
-#
-0 4	* * *	root	[ -x /usr/bin/protontricks_maintenance ] && /usr/bin/protontricks_maintenance
